3c4daf3e7752dbcd8f84646451924d91b220c835
Peder Aa. Hoff <peder.aa.hoff@gmail.com>
2025-03-26 13:15:56 +0100

    La inn et stygt bakgrunnsbilde

--- backgroundGenerator.cpp
+++ backgroundGenerator.cpp
@@ -1,8 +1,11 @@
 #include "backgroundGenerator.h"
-#include "animationwindow.h"
+#include "AnimationWindow.h"
 
+TDT4102::Image image("images/background2.png");
 void drawBackground(TDT4102::AnimationWindow& window){
-    TDT4102::Color grassColor = TDT4102::Color::green;
+    //TDT4102::Color grassColor = TDT4102::Color::green;
+    //window.draw_rectangle({0, window.height()-100}, window.width(), 100, grassColor);
 
-    window.draw_rectangle({0, window.height()-100}, window.width(), 100, grassColor);
+    TDT4102::Point topLeftCorner {0, 0};
+    window.draw_image(topLeftCorner, image, window.width(), window.height());
 }
