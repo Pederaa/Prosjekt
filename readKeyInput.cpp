14d189239e232d7aad8f36e6c00d51d2b021313b
Peder Aa. Hoff <peder.aa.hoff@gmail.com>
2025-04-08 18:46:05 +0200

    Kommentarer

--- readKeyInput.cpp
+++ readKeyInput.cpp
@@ -4,6 +4,7 @@
 
 #include <unordered_map>
 
+// Map som kartlegger alle keyinputsene til sine char-verdier
 std::unordered_map<KeyboardKey, char> keyMap = {
     {KeyboardKey::A, 'A'}, {KeyboardKey::B, 'B'}, {KeyboardKey::C, 'C'}, {KeyboardKey::D, 'D'}, {KeyboardKey::E, 'E'},
     {KeyboardKey::F, 'F'}, {KeyboardKey::G, 'G'}, {KeyboardKey::H, 'H'}, {KeyboardKey::I, 'I'}, {KeyboardKey::J, 'J'},
@@ -16,37 +17,42 @@ std::unordered_map<KeyboardKey, char> keyMap = {
     {KeyboardKey::KEY_8, '8'}, {KeyboardKey::KEY_9, '9'}
 };
 
-static bool isKeyPressed = false;
+// Dersom spilleren trykker på en knapp, skal den verdien skrives til typeText, 
+// men bare èn gang. Dersom spilleren løfter fingeren av tasten og trykker den inn igjen, 
+// eller trykker på en annen knapp skal en ny verdi skirves inn. 
+
+//Verdi som husker om knappen var inne ved forrige iterering.
+static bool isKeyPressed = false; 
+
+//Variabel som husker hva den forrige nøkkelen som ble trykket var. 
 static KeyboardKey prevKey = KeyboardKey::SPACE;
+
 void getCharInput(TDT4102::AnimationWindow& window, std::string& typeText){
-    if (window.is_key_down(KeyboardKey::BACKSPACE)){
+    //Backspace er den eneste spesielle tasten. Den sletter siste element i typetext. 
+    if (window.is_key_down(KeyboardKey::BACKSPACE)){ 
         if((!isKeyPressed || prevKey != KeyboardKey::BACKSPACE) && !typeText.empty()){
             typeText.pop_back();
-        }
 
+            if (checkIfGuessIsCorrect(typeText)){ 
+                typeText = "";
+            }
+        }
         prevKey = KeyboardKey::BACKSPACE;
         isKeyPressed = true;
         return;
     }
 
-    
-    // else if (window.is_key_down(KeyboardKey::ENTER)){
-    //     if((!isKeyPressed || prevKey != KeyboardKey::ENTER) && !typeText.empty()){
-    //         checkIfGuessIsCorrect(typeText);
-    //         typeText = "";
-    //     }
-    //     prevKey = KeyboardKey::ENTER;
-    //     isKeyPressed = true;
-    //     return;
-    // }
-
-    for (const auto& [key, value] : keyMap) {
+    // Her sjekkes alle tastene en etter en i keymappet. Dersom tasten er nede og 
+    // den ikke var nede siden sist (eller evt. om det brukeren har trykket på en annen 
+    // knapp så raskt at det ikke ble merket) skal tasten legges til
+    for (const auto& [key, value] : keyMap) { 
         if (window.is_key_down(key)) {
             if (!isKeyPressed || key != prevKey){
                 typeText += value;
 
-                bool d = checkIfGuessIsCorrect(typeText);
-                if (d){
+                // Her sjekker vi om det brukeren har skrevet inn matcher med noen av 
+                // bombene i lufta. Da resettes innholdet i typeText
+                if (checkIfGuessIsCorrect(typeText)){ 
                     typeText = "";
                 }
             }
@@ -56,5 +62,6 @@ void getCharInput(TDT4102::AnimationWindow& window, std::string& typeText){
         }
     }
 
+    // Dersom ingen av return-statementsene har blitt aktivert, skal denne variabelen bli false
     isKeyPressed = false;
-}
+    }
